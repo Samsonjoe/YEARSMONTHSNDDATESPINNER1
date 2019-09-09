a6148668087d5456ce879258ca19ffde843e3f43
Evans Wakae <wakaee@DESKTOP-SVAQSR9.aonkearsnbo.ke.aon.bz>
2019-09-09 12:01:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/yearsmonthsnddatespinner/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.yearsmonthsnddatespinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+}
